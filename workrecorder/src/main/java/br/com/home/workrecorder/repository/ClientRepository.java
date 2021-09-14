58431b7fc778712d10aade3f7a6ced32a30f4cba
vielGuilherme <48812620+vielGuilherme@users.noreply.github.com>
2021-09-13 21:35:35 -0300

    Delete workrecorder directory

--- workrecorder/src/main/java/br/com/home/workrecorder/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.home.workrecorder.repository;
-
-import br.com.home.workrecorder.models.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<Client, Long> {
-    Client findByName(String name);
-}
--- workrecorder/src/main/java/br/com/home/workrecorder/repository/WorkRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.home.workrecorder.repository;
-
-import br.com.home.workrecorder.models.Work;
-import org.springframework.data.repository.CrudRepository;
-
-public interface WorkRepository extends CrudRepository<Work, Long> {
-}
